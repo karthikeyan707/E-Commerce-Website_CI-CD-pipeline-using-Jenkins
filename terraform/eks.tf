42b4e8e079b61c67ad617abe8c1b0b73c350286d
karthikeyan <karthikeyanuk559@gmail.com>
2026-04-05 19:42:42 +0530

    terraform_update

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -21,7 +21,6 @@ resource "aws_eks_cluster" "eks" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name             = aws_eks_cluster.eks.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.28.0-eksbuild.1"
   # Using node role IAM permissions (AmazonEBSCSIDriverPolicy attached in iam.tf)
 
   depends_on = [
